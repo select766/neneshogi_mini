97f51af9387c26069069340ea88255b7939d5394
select766 <select766@outlook.jp>
2018-04-03 21:33:22 +0900

    同じ場所に移動したときに「同歩」のように表示

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,13 @@ import { KifuTool } from "./kifu_tool";
 let think = new Think();
 think.load();
 
+let last_move = null;
 async function next() {
     let best_move = await think.do_think(pos);
     if (!best_move) {
         console.log("Mated");
     } else {
-        console.log(`Move: ${KifuTool.GetMoveString(pos, best_move)}`);
+        console.log(`Move: ${KifuTool.GetMoveString(pos, best_move, last_move)}`);
         if (best_move.kind) {
             // drop
             pos.drop(best_move.to.x, best_move.to.y, best_move.kind);
@@ -22,6 +23,7 @@ async function next() {
                 best_move.to.x, best_move.to.y, best_move.promote);
         }
     }
+    last_move = best_move;
     console.log(pos.toCSAString());
     console.log(`Check: ${pos.isCheck()}`);
 }
--- src/kifu_tool.ts
+++ src/kifu_tool.ts
@@ -23,17 +23,24 @@ const zenkakusuji = "０１２３４５６７８９";
 const kansuji = "〇一二三四五六七八九";
 
 export class KifuTool {
-    public static GetMoveString(pos: Shogi, move: IMovePromote) {
+    public static GetMoveString(pos: Shogi, move: IMovePromote, last_move?: IMovePromote) {
         let kind = move.kind;
         if (!kind) {
             kind = pos.get(move.from.x, move.from.y).kind;
         }
-        let str = `${zenkakusuji[move.to.x]}${kansuji[move.to.y]}${pieceKindKifuName[kind]}`;
+        let str = `${zenkakusuji[move.to.x]}${kansuji[move.to.y]}`;
         if (move.kind) {
             // 打ち
+            str += pieceKindKifuName[kind];
             str += "打";
         } else {
             // 移動
+            if (last_move) {
+                if (move.to.x === last_move.to.x && move.to.y === last_move.to.y) {
+                    str += "同";
+                }
+            }
+            str += pieceKindKifuName[kind];
             if (move.promote) {
                 str += "成";
             }
